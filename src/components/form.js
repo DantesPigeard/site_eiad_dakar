a54e69abb744a9951435246ce465e9d6a3067bed
Stan le Punk <stanislasiii@gmail.com>
2021-07-14 17:03:29 +0200

    Update form.js

--- src/components/form.js
+++ src/components/form.js
@@ -1,9 +1,14 @@
 import * as React from "react";
+// https://www.gatsbyjs.com/docs/building-a-contact-form/
 
+// Il y a un gestionnaire de pourriel sur Netlify
+// https://docs.netlify.com/forms/setup/
+// https: www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/#form-handling-with-static-site-generators
 export function Form({ name, children }) {
   return (
     <>
       <form name={name} method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value={name} />
         <p>
           <label>
             <input type="text" name="family name" placeHolder="nom" />
