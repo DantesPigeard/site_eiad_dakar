37d24bccbc6d766af70628cc3fef31eac48417e7
Stan le Punk <stanislasiii@gmail.com>
2021-08-11 19:55:21 +0200

    alignement

--- src/components/header.js
+++ src/components/header.js
@@ -43,7 +43,9 @@ export function Header() {
         </Button>
       </section>
       <section className="h_cell">
-        <Button where="/inscription">s'inscrire</Button>
+        <Button where="/inscription">
+          <div className="h_text">s'inscrire</div>
+        </Button>
       </section>
       {/* <section className="h_cell">
         <Button where="/contact">contact</Button>
